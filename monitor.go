6c19994655c910c6d228789378a23daf7e87aba2
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-03-13 10:29:02 +0100

    CLI handling initialization

--- monitor.go
+++ monitor.go
@@ -17,9 +17,26 @@ limitations under the License.
 package main
 
 import (
+	"bufio"
+	"flag"
 	"fmt"
+	"os"
+
+	"github.com/c-bata/go-prompt"
+	"github.com/logrusorgru/aurora"
 	"github.com/spf13/viper"
+
+	"github.com/RedHatInsights/ccx-data-pipeline-monitor/commands"
 )
+
+var colorizer aurora.Aurora
+
+func executor(t string) {
+}
+
+func completer(in prompt.Document) []prompt.Suggest {
+	return nil
+}
 
 func main() {
 	// read configuration first
@@ -29,5 +46,27 @@ func main() {
 	err := viper.ReadInConfig()
 	if err != nil {
 		panic(fmt.Errorf("Fatal error config file: %s", err))
+	}
+
+	// parse command line arguments and flags
+	var colors = flag.Bool("colors", true, "enable or disable colors")
+	var useCompleter = flag.Bool("completer", true, "enable or disable command line completer")
+	// var askForConfirmation = flag.Bool("confirmation", true, "enable or disable asking for confirmation for selected actions (like delete)")
+	flag.Parse()
+
+	colorizer = aurora.NewAurora(*colors)
+	commands.SetColorizer(colorizer)
+
+	if *useCompleter {
+		p := prompt.New(executor, completer)
+		p.Run()
+	} else {
+		scanner := bufio.NewScanner(os.Stdin)
+		fmt.Print("> ")
+		for scanner.Scan() {
+			line := scanner.Text()
+			executor(line)
+			fmt.Print("> ")
+		}
 	}
 }
