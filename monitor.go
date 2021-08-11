353998f514c96a1f72050dadfa8c8b2b1d02cb6b
Pavel Tisnovsky <ptisnovs@redhat.com>
2021-08-11 11:37:41 +0200

    Link to generated documentation added into monitor.go

--- monitor.go
+++ monitor.go
@@ -16,6 +16,9 @@ limitations under the License.
 
 package main
 
+// Documentation in literate-programming-style is available at:
+// https://redhatinsights.github.io/ccx-data-pipeline-monitor/packages/monitor.html
+
 import (
 	"bufio"
 	"context"
