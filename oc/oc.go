04fe136db65d7c365dc57341843776a7c7bd6f13
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-10-19 16:17:36 +0200

    Syntax fix

--- oc/oc.go
+++ oc/oc.go
@@ -35,9 +35,9 @@ func Command(args ...string) (outString, errString string, err error) {
 	cmd.Stdout = &stdout
 	cmd.Stderr = &stderr
 
-	err := cmd.Run()
-	outString := stdout.String()
-	errString := stderr.String()
+	err = cmd.Run()
+	outString = stdout.String()
+	errString = stderr.String()
 	if err != nil {
 		return outString, errString, err
 	}
